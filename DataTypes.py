2b6d5bd4c8883e0a47a6a19e8e31701f7b51baff
SVIATOSLAV\Sviatoslav <boichuk.sv@mail.ru>
2017-04-28 11:59:37 +0300

    isinstance method

--- DataTypes.py
+++ DataTypes.py
@@ -1,17 +1,31 @@
 # Python data types
 
-def CompareObjects(a , b):
+
+def CompareObjects(a, b):
     if a == b:
         return "equal as data"
     elif a is b:
         return "equal as referense"
     elif type(a) is type(b):
         return "equal as data type"
-    else: return "not equal"
+    else:
+        return "not equal"
 
 a = 12
 b = a
 
-msg = CompareObjects(a,b)
+msg = CompareObjects(a, b)
 
 print(msg)
+print(type(msg))
+
+# isinstance - перевірка типу змінної (враховує наслідування)
+
+arr = [1,2,3]
+
+if isinstance(arr,list):
+    arr.append(4)
+
+for a in arr:
+    print(a**2)
+
