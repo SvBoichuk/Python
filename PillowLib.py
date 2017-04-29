13723aab8d47439ee8cd1b4c7c7bd0e1db1afc81
SVIATOSLAV\Sviatoslav <boichuk.sv@mail.ru>
2017-04-29 17:25:55 +0300

    Pillow update

--- PillowLib.py
+++ PillowLib.py
@@ -2,14 +2,27 @@ from PIL import Image
 
 jpgImage = Image.open("world.jpg")
 
-area = (100,100, 300, 375)
+#area = (100,100, 300, 375)
 
 print(jpgImage.size)
 print(jpgImage.format)
 
-cropped_img = jpgImage.crop(area)
+#cropped_img = jpgImage.crop(area)
+#cropped_img.save('new.jpg')
 
-cropped_img.save('new.jpg')
+picture_size = jpgImage.size
+print("size: ", picture_size)
 
-jpgImage.show();
+color_mode = jpgImage.mode
+print("color mode = ", color_mode)
+
+red, green, blue = jpgImage.split()
+
+newImage = Image.merge("RGB", (green, blue, red))
+newImage.show()
+
+transponseImage = jpgImage.transpose(Image.FLIP_LEFT_RIGHT)
+transponseImage.show()
+
+#jpgImage.show();
 
