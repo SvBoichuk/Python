1dcfd37e296446a50a9f2048052096285ab4b00d
SVIATOSLAV\Sviatoslav <boichuk.sv@mail.ru>
2017-05-01 13:27:25 +0300

    OLOLO

--- Operations.py
+++ Operations.py
@@ -14,3 +14,27 @@ total = sum(retPair)
 
 print("Total = ", total)
 print("Len = ", len(retPair))
+
+array = [1,2,3,4,5,6]
+
+print("Seek ", array[1 : 5 : 2])
+
+add2Arrys = list(retPair) + array
+
+print("new arr = ", add2Arrys)
+
+
+def PrintStringN(string, count):
+    if count > 0:
+        string *= count
+    print(string)
+
+
+count = int(input("Enter repeat count: "))
+string = input("Enter string: ")
+
+PrintStringN(string, count)
+
+
+
+
