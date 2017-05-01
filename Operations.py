0b6f53c941449957eef2f46c8bd8f3cb474fae89
SVIATOSLAV\Sviatoslav <boichuk.sv@mail.ru>
2017-05-01 13:19:20 +0300

    Python operators

---
+++ Operations.py
@@ -0,0 +1,16 @@
+retPair = divmod(5,2)
+
+print("divmod = ", retPair)
+
+# div = retPair[0]
+# mod = retPair[1]
+
+div, mod = retPair
+
+# C - style print format???
+print("Div = %d, mod = %d", div, mod)
+
+total = sum(retPair)
+
+print("Total = ", total)
+print("Len = ", len(retPair))
